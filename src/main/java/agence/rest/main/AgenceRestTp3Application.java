83c3150addef312175c64339e7d922c0440c0e84
Miguel-Delhelle <miguel.delhelle@gmail.com>
2025-01-05 18:27:16 +0100

    first commit

---
+++ src/main/java/agence/rest/main/AgenceRestTp3Application.java
@@ -0,0 +1,13 @@
+package agence.rest.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenceRestTp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenceRestTp3Application.class, args);
+	}
+
+}
---
+++ src/test/java/agence/rest/main/AgenceRestTp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package agence.rest.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenceRestTp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
